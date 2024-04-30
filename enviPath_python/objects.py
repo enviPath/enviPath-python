7498f7cb0dbda7de111294e880cfbfe204e02e38
Albert Anguera Sempere <albert.anguerasempere@uzh.ch>
2024-04-30 08:26:38 +0200

    Edit docstring

--- enviPath_python/objects.py
+++ enviPath_python/objects.py
@@ -842,7 +842,7 @@ class Scenario(enviPathObject):
 
     def get_linked_objects(self) -> List['ReviewableEnviPathObject']:
         """
-        Gets the objects that are linked to the current scenario
+        Gets the objects that are linked to the current scenario.
 
         :return: A list of objects that have this scenario attached
         :rtype: List
